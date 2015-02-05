588339031559a470529cd03b74b5a4c340a29a1b
Diego Castronuovo <castronu>
2015-02-05 22:52:40 +0100

    CLIFF-3Log tail works in async way using mongo collection.

--- src/main/resources/scripts/core.js
+++ src/main/resources/scripts/core.js
@@ -1,39 +1,41 @@
-
-
 // simple-todos.js
 if (Meteor.isClient) {
 
 
 
 
+        Template.logss.logs = function(){
+            return LOGSS.find();
+        }
+
 // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
-
-
 
 
 // on the client
     Meteor.subscribe('logss');
+
+
+    LOGSS = new Mongo.Collection("logs");
 
     Template.commands.events({
         "submit .new-task": function (event) {
             // This function is called when the new task form is submitted
             var command = "";
 
-            $.each($('#myform').serializeArray(), function() {
+            $.each($('#myform').serializeArray(), function () {
                 command += this.value + " ";
             });
 
             console.log(command);
 
 
-            Meteor.call("run", command, function(err, response) {
+            Meteor.call("run", command, function (err, response) {
                 Session.set('code', response);
 
                 console.log(response);
                 console.log(err);
                 Session.set("logss", response);
             });
-
 
 
             // Prevent default form submit
@@ -49,26 +51,82 @@ if (Meteor.isServer) {
 
     LOGSS = new Mongo.Collection("logs");
 
+    Meteor.startup(function() {
+
+        return Meteor.methods({
+
+            removeAllPosts: function() {
+
+                return LOGSS.remove({});
+
+            }
+
+        });
+
+    });
+
+
+    Meteor.publish("logss", function () {
+        return LOGSS.find();
+    });
+
+
+    Fiber = Npm.require('fibers');
+
+    Tail = Npm.require('tail').Tail;
+
+    tail = new Tail("/tmp/mylog.log");
+    var count=0;
+    tail.on("line", function (data) {
+        console.log(data);
+        var document = {
+            id: count,
+            content: data,
+            details: "dddddd"
+        };
+        console.log(count);
+
+        Fiber(function () {
+            LOGSS.insert(document);
+        }).run();
+
+        //Cerca tutti i log...
+        Fiber(function () {
+
+            var cursor = LOGSS.find({id: count}, {content: 1, _id: 1});
+            LOGSS.find().forEach(function(doc,index,cursor){
+                console.log(doc.content);
+            });
+
+
+        }).run();
+        count++;
+    });
+
+    tail.on("error", function (error) {
+        console.log('ERROR: ', error);
+    });
+
 
     // load future from fibers
-    var Future=Npm.require("fibers/future");
+    var Future = Npm.require("fibers/future");
 // load exec
-    var exec=Npm.require("child_process").exec;
+    var exec = Npm.require("child_process").exec;
 
 
     Meteor.methods({
-        run: function(command) {
+        run: function (command) {
 
             Tail = Npm.require('tail').Tail;
 
             tail = new Tail("/tmp/mylog.log");
 
-            tail.on("line", function(data) {
+            tail.on("line", function (data) {
                 console.log(data);
 
             });
 
-            tail.on("error", function(error) {
+            tail.on("error", function (error) {
                 console.log('ERROR: ', error);
             });
 
@@ -77,18 +135,18 @@ if (Meteor.isServer) {
             var fut = new Future();
 
             // This should work for any async method
-            setTimeout(function() {
+            setTimeout(function () {
 
                 // Return the results
                 fut.return(" (delayed for 3 seconds)");
 
             }, 3 * 1000);
 
-            exec(command,function(error,stdout,stderr){
-                if(error){
+            exec(command, function (error, stdout, stderr) {
+                if (error) {
                     console.log(error);
                     console.log(stderr.toString());
-                    throw new Meteor.Error(500,command+" failed");
+                    throw new Meteor.Error(500, command + " failed");
                 }
                 console.log(stdout.toString())
                 fut.return(stdout.toString());
@@ -100,9 +158,6 @@ if (Meteor.isServer) {
             return fut.wait();
         }
     });
-
-
-
 
 
 }
